67b8ac0c61f7af95f590fc7af674d6b8af905253
kalin <kalindusupun@gmail.com>
2021-09-15 18:31:52 +0530

    Movie-Streaming-App

---
+++ app/src/main/java/com/moviestreamingapp/retrofit/Credentials.java
@@ -0,0 +1,8 @@
+package com.moviestreamingapp.retrofit;
+
+public class Credentials {
+
+    public static final String BASE_URl="https://api.themoviedb.org/3";
+    public static final String API_KEY="459351d1311a26f26c93016a0d788db7";
+
+}
